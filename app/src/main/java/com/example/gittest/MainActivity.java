ec231341a29789e5f8717d823179d4e29ed2f9fc
Home <praveen7512>
2021-05-25 09:46:18 -0400

    Toast commit 2 test by praveen

--- app/src/main/java/com/example/gittest/MainActivity.java
+++ app/src/main/java/com/example/gittest/MainActivity.java
@@ -13,5 +13,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toast.makeText(this,"hello",Toast.LENGTH_SHORT).show();
+        Toast.makeText(this,"commit 2 has been done",Toast.LENGTH_SHORT).show();
     }
 }
